a9f99a4be1353cb6ffaddca7bc2246a8dfd88720
Pia <achigarpia@gmail.com>
2021-04-24 12:42:31 -0300

    ItemCount

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,10 +1,26 @@
 import "./ItemCount.scss";
+//este comp va a una Card y la Card va a ItemListCarrito
+import { useState, useEffect } from "react";
 
 
-export default function ItemCount( stock, initial, onAdd){ 
-  
+export default function ItemCount( stock, initial, onAdd){
+  const [count , setCount] = useState(1);
+  const sumar = ()=>{
+    setCount(count + 1);
+  };
+  const restar = () =>{
+    setCount(count -1);
+  };
+  useEffect(()=>{
+    if(count < 1){
+      alert("Stock 0!")
+    }
+  }, [count]);
   return(
       <div className="count">
+        <button onClick={restar}>-</button>
+        <h2>{count}</h2>
+        <button onClick={sumar}>+</button>
       </div>
   )
 }
--- src/components/ItemListCarrito/ItemListCarrito.js
+++ src/components/ItemListCarrito/ItemListCarrito.js
@@ -1,13 +1,17 @@
 import "./ItemListCarrito.scss";
+import ItemCount from "../ItemCount/ItemCount";
 import Cartwidget from "../Cartwidget/Cartwidget";
 
 export default function ItemListCarrito(){ // Links
   //aca tengo q poner las fotos del carrito (Perfumer/Dermocosm/etc)
   return(
-      <div className="carrito-titulo">
-        <Cartwidget className="carro"/>
-        <h3> Tu Carrito </h3>
-        <h4>Vacio</h4>
+    <div className="div-carro">
+        <div className="carrito-titulo">
+          <Cartwidget className="carro"/>
+          <h3>Tu Carrito</h3>
+          <h4>Vacio</h4>
+        </div>
+        <div className="ItemCount"><ItemCount/></div>
       </div>
   )
 }
