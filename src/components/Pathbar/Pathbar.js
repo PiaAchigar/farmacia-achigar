c5d5c010fef20c07f5c70ae03c55ce04533c9ff1
Pia <achigarpia@gmail.com>
2021-06-03 18:35:30 -0300

    limpieza

--- src/components/Pathbar/Pathbar.js
+++ src/components/Pathbar/Pathbar.js
@@ -1,15 +1,19 @@
 import "./Pathbar.scss";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const Pathbar = ({ruta}) => {
-    return(
-        <header>
-            <nav>
-                <p><Link to="/" className="home">Home</Link>/{ruta}</p>
-            </nav>
-
-        </header>
-    )
-}
+const Pathbar = ({ ruta }) => {
+  return (
+    <header>
+      <nav>
+        <p>
+          <Link to="/" className="home">
+            Home
+          </Link>
+          /{ruta}
+        </p>
+      </nav>
+    </header>
+  );
+};
 
 export default Pathbar;
